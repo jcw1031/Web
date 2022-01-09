c31061594ba9d2251ff2ce1e76c7ffd9018e9a11
woopaca <jcw001031@gmail.com>
2022-01-09 20:32:56 +0900

    initial commit

---
+++ momentum/app.js
@@ -0,0 +1,39 @@
+let a = 5;
+let b = 2;
+let myNmae = 'JICHANWOO';
+
+console.log('hello ' + myNmae);
+
+console.log(a+b);
+console.log(a/b);
+console.log(a*b);
+console.log(a-b);
+
+const amIFat = null
+let something;
+console.log(something, amIFat);
+
+//array
+const daysOfWeek = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
+
+console.log(daysOfWeek);
+console.log(daysOfWeek[3]);
+
+daysOfWeek.push('sum');
+console.log(daysOfWeek);
+
+const toBuy = ['potato', 'tomato',  'pizza'];
+
+//object
+const player = {
+    name: "jcw",
+    points: 10,
+    fat: false,
+}
+console.log(player);
+console.log(player.name);
+console.log(player['name']);
+
+player.lastName = 'potato';
+console.log(player);
+
